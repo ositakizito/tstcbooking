82be9395a81718fd38a08353670c676583b6d89d
osita <70627592+ositakizito@users.noreply.github.com>
2024-03-28 03:00:21 +0100

    Update login.js

--- src/components/login.js
+++ src/components/login.js
@@ -13,7 +13,7 @@ export default function Login() {
     const handlesubmit = (e)=> {
       e.preventDefault();
       
-      axios.post('http://localhost:3001/users', value)
+      axios.post('https://tstcserver.onrender.com/users', value)
       .then(res => {
         navigate("/profile");
       })
