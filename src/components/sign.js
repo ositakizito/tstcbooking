3d41a9c02339a25a36d660dd1606cc20445170c0
osita <70627592+ositakizito@users.noreply.github.com>
2024-05-08 18:03:26 +0100

    Update sign.js

--- src/components/sign.js
+++ src/components/sign.js
@@ -29,7 +29,7 @@ export default function SignUp() {
       setLoading(true);
 
       axios
-      .post("https://tstcserver.onrender.com", value)
+      .post("https://tstcserver.onrender.com/users", value)
       .then((res) => {
         setSuccessMessage("Sign up successful. Redirecting to login...");
         setTimeout(() => {
